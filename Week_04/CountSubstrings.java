76466761c994fbbc770b60ffd91d19ce341396fb
van <958423732@qq.com>
2020-08-09 23:56:33 +0800

    Update the homework in week_4

---
+++ Week_04/CountSubstrings.java
@@ -0,0 +1,31 @@
+package Java.week_4;
+//647. 回文子串
+public class CountSubstrings {
+    public int countSubstrings(String s) {
+        if(s == null || s.equals("")){
+           return 0;
+       }
+       int n = s.length();
+       boolean[][] dp = new boolean[n][n];
+       int result = s.length();
+       for(int i = 0; i<n; i++) dp[i][i] = true;
+       for(int i = n-1; i>=0; i--){
+           for(int j = i+1; j<n; j++){
+               if(s.charAt(i) == s.charAt(j)) {
+                   if(j-i == 1){
+                       dp[i][j] = true;
+                   }
+                   else{
+                       dp[i][j] = dp[i+1][j-1]; 
+                   }
+               }else{
+                   dp[i][j] = false;
+               }
+               if(dp[i][j]){
+                   result++;
+               }
+           }
+       }
+       return result;
+   }
+}
---
+++ Week_04/NumDecodings.java
@@ -0,0 +1,31 @@
+package Java.week_4;
+//91. 解码方法
+public class NumDecodings {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int len = s.length();
+
+        int help = 1;
+        int res = 0;
+        if (s.charAt(len - 1) != '0') {
+            res = 1;
+        }
+        for (int i = len - 2; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                help = res;
+                res = 0;
+                continue;
+            }
+            if ((s.charAt(i) - '0') * 10 + (s.charAt(i + 1) - '0') <= 26) {
+                res += help;
+                help = res-help;
+            } else {
+                help = res;
+            }
+
+        }
+        return res;
+    }
+}
